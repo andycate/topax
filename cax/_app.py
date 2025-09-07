eaa78575c33da8f944f7e594333cccd86824c58e
Andy Cate <and164v@gmail.com>
2025-09-06 21:39:33 -0700

    reorg a bit

--- cax/_app.py
+++ cax/_app.py
@@ -13,141 +13,186 @@ import threading
 import queue
 import time
 from IPython import embed
+import argparse
+from pathlib import Path
 
-from cax._utils import redraw, rotation_matrix, rotation_matrix_about_vector
+from cax._utils import redraw_ortho, rotation_matrix, rotation_matrix_about_vector, _dummy_sdf, normalize
 from cax.sdfs import test_sdf
 
-# global defs
-window_width, window_height = 800, 600
-fb_width, fb_height = None, None
-frame_buf = None # numpy array for texture
-dragging = False
-last_pos_x, last_pos_y = 0, 0
-cam_pose = np.array([3.0, 4.0, 5.0]) * 0.5
-looking_at = np.array([0., 0., 0.])
-world_up = np.array([0., 0., 1.])
-zoom = 0.8
-sdf = test_sdf()
+class SceneHandler:
+    def __init__(self, window):
+        self._window = window
+        self._fb_width, self._fb_height = glfw.get_framebuffer_size(window)
+        self._frame_buf = None
+        self._texture_id = None
+        self._sdf = test_sdf()
+        self._hifi_render = None
+        self._lofi_render = None
+        self._camera_position = np.array([0.0, 0.0, 1.0])
+        self._camera_up = np.array([0.0, 1.0, 0.0])
+        self._looking_at = np.array([0.0, 0.0, 0.0])
+        self._fx = 1.0
 
-command_queue = queue.Queue()
+        self._make_texture()
+        self._gen_render_funcs()
 
-def make_texture(texture_id=None):
-    global frame_buf, fb_width, fb_height
+    def __del__(self):
+        gl.glDeleteTextures(1, [self._texture_id])
 
-    frame_buf = np.zeros((fb_height, fb_width, 4), dtype=np.float32)
-    gl.glViewport(0, 0, fb_width, fb_height)
-    gl.glEnable(gl.GL_BLEND)
-    gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
-    # Generate a new texture
-    if texture_id is None:
-        texture_id = gl.glGenTextures(1)
-    gl.glBindTexture(gl.GL_TEXTURE_2D, texture_id)
+    def _gen_render_funcs(self):
+        self._hifi_render = redraw_ortho.trace(self._sdf, self._fb_width, self._fb_height, 100, self._camera_position, self._camera_up, self._looking_at, self._fx).lower().compile()
+        self._lofi_render = redraw_ortho.trace(self._sdf, self._fb_width, self._fb_height, 30, self._camera_position, self._camera_up, self._looking_at, self._fx).lower().compile()
 
-    # Set texture parameters
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+    def _make_texture(self):
+        self._frame_buf = np.zeros((self._fb_height, self._fb_width, 4), dtype=np.float32)
+        gl.glViewport(0, 0, self._fb_width, self._fb_height)
+        gl.glEnable(gl.GL_BLEND)
+        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+        # Generate a new texture
+        if self._texture_id is None:
+            self._texture_id = gl.glGenTextures(1)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self._texture_id)
 
-    # Upload the NumPy array data to the texture
-    # The image is upside-down in OpenGL, so a flip is often needed.
-    # PIL's `transpose` is one way, or you can handle it with the texture matrix.
-    # Here we assume the array is already oriented correctly or will be corrected elsewhere.
-    gl.glTexImage2D(
-        gl.GL_TEXTURE_2D,
-        0,
-        gl.GL_RGBA32F,
-        fb_width,
-        fb_height,
-        0,
-        gl.GL_RGBA,
-        gl.GL_FLOAT,
-        frame_buf,
-    )
-    return texture_id
+        # Set texture parameters
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
 
-def draw_scene(texture_id, fast=False):
-    global frame_buf, dragging, last_pos_x, last_pos_y, cam_pose, window_width, window_height, fb_width, fb_height, sdf, world_up, looking_at, zoom
-
-    np.copyto(
-        frame_buf, 
-        np.array(
-            redraw(sdf, fb_width, fb_height, 20 if fast else 100, cam_pose, world_up, looking_at, zoom, jnp.array([0.8, 0.2, 0.1])), 
-            copy=False, 
-            dtype=np.float32
+        gl.glTexImage2D(
+            gl.GL_TEXTURE_2D,
+            0,
+            gl.GL_RGBA32F,
+            self._fb_width,
+            self._fb_height,
+            0,
+            gl.GL_RGBA,
+            gl.GL_FLOAT,
+            self._frame_buf,
         )
-    )  # get new frame from JAX
+        return self._texture_id
     
-    gl.glClearColor(0.2, 0.2, 0.2, 1.0)
-    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+    def update_sdf(self, sdf):
+        self._sdf = sdf
+        self._gen_render_funcs()
 
-    gl.glBindTexture(gl.GL_TEXTURE_2D, texture_id)
-    gl.glTexSubImage2D(
-        gl.GL_TEXTURE_2D,
-        0,
-        0, 0,
-        fb_width, fb_height,
-        gl.GL_RGBA,
-        gl.GL_FLOAT,
-        frame_buf,
-    )
+    def update_fb_size(self):
+        self._fb_width, self._fb_height = glfw.get_framebuffer_size(self._window)
+        self._make_texture()
+        self._gen_render_funcs()
 
-    # Bind the texture to display it
-    gl.glEnable(gl.GL_TEXTURE_2D)
-    gl.glEnable(gl.GL_BLEND)
-    gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
-    gl.glColor3f(1.0, 1.0, 1.0)
+    def rotate_2d(self, dx, dy):
+        cam_right = normalize(np.linalg.cross(-self._camera_position, self._camera_up))
+        x_rot = rotation_matrix_about_vector(-dx / 300., self._camera_up)
+        y_rot = rotation_matrix_about_vector(-dy / 300., cam_right)
+        self._camera_position = x_rot @ self._camera_position
+        self._camera_position = y_rot @ self._camera_position
+        self._camera_up = y_rot @ self._camera_up
 
-    # Draw a quad to display the texture
-    gl.glBegin(gl.GL_QUADS)
-    gl.glTexCoord2f(0, 0)
-    gl.glVertex2f(-1, -1)
-    gl.glTexCoord2f(1, 0)
-    gl.glVertex2f(1, -1)
-    gl.glTexCoord2f(1, 1)
-    gl.glVertex2f(1, 1)
-    gl.glTexCoord2f(0, 1)
-    gl.glVertex2f(-1, 1)
-    gl.glEnd()
+    def zoom(self, delta):
+        self._fx *= (1+delta * 0.008)
+        
 
-    gl.glDisable(gl.GL_TEXTURE_2D)
+    def draw_scene(self, fast=False):
+        """
+        This function is responsible for drawing all parts of the scene. It will take in the 
+        """
+        if fast:
+            np.copyto(
+                self._frame_buf, 
+                np.array(
+                    self._lofi_render(self._camera_position, self._camera_up, self._looking_at, self._fx), 
+                    copy=False, 
+                    dtype=np.float32
+                )
+            )
+        else:
+            np.copyto(
+                self._frame_buf, 
+                np.array(
+                    self._hifi_render(self._camera_position, self._camera_up, self._looking_at, self._fx), 
+                    copy=False, 
+                    dtype=np.float32
+                )
+            )
+        
+        gl.glClearColor(0.2, 0.2, 0.2, 1.0)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self._texture_id)
+        gl.glTexSubImage2D(
+            gl.GL_TEXTURE_2D,
+            0,
+            0, 0,
+            self._fb_width, self._fb_height,
+            gl.GL_RGBA,
+            gl.GL_FLOAT,
+            self._frame_buf,
+        )
+
+        # Bind the texture to display it
+        gl.glEnable(gl.GL_TEXTURE_2D)
+        gl.glEnable(gl.GL_BLEND)
+        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+        gl.glColor3f(1.0, 1.0, 1.0)
+
+        # Draw a quad to display the texture
+        gl.glBegin(gl.GL_QUADS)
+        gl.glTexCoord2f(0, 0)
+        gl.glVertex2f(-1, -1)
+        gl.glTexCoord2f(1, 0)
+        gl.glVertex2f(1, -1)
+        gl.glTexCoord2f(1, 1)
+        gl.glVertex2f(1, 1)
+        gl.glTexCoord2f(0, 1)
+        gl.glVertex2f(-1, 1)
+        gl.glEnd()
+
+        gl.glDisable(gl.GL_TEXTURE_2D)
+
+        glfw.swap_buffers(self._window)
 
 def main():
-    global frame_buf, dragging, last_pos_x, last_pos_y, cam_pose, window_width, window_height, fb_width, fb_height, sdf, world_up, looking_at, zoom
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir", help="top level directory of CAD project")
+    args = parser.parse_args()
+    project_dir = Path(args.dir)
+
+    if not project_dir.is_dir():
+        raise FileNotFoundError(f"Can't find project dir {project_dir}")
 
     if not glfw.init():
         raise RuntimeError("Failed to initialize GLFW")
 
-    window = glfw.create_window(window_width, window_height, "CAX", None, None)
+    window = glfw.create_window(800, 600, "CAX", None, None)
     if not window:
         glfw.terminate()
         raise RuntimeError("Failed to create GLFW window")
 
     glfw.make_context_current(window)
 
-    fb_width, fb_height = glfw.get_framebuffer_size(window)
+    scene = SceneHandler(window)
 
-    texture_id = make_texture()
-    draw_scene(texture_id)
-    glfw.swap_buffers(window)
-
-    # Mouse state
+    dragging = False
+    last_pos_x, last_pos_y = 0, 0
     def mouse_button_callback(win, button, action, mods):
-        global dragging, last_pos_x, last_pos_y
+        nonlocal dragging, last_pos_x, last_pos_y
         if button == glfw.MOUSE_BUTTON_LEFT:
             dragging = (action == glfw.PRESS)
             last_pos_x, last_pos_y = glfw.get_cursor_pos(window)
             if not dragging:
-                draw_scene(texture_id, fast=False)
-                glfw.swap_buffers(window)
+                scene.draw_scene(fast=False)
 
     glfw.set_mouse_button_callback(window, mouse_button_callback)
 
     def window_resize_callback(win, width, height):
-        global fb_width, fb_height
-        fb_width, fb_height = glfw.get_framebuffer_size(window)
-        gl.glViewport(0, 0, fb_width, fb_height)
-        make_texture(texture_id)
+        scene.update_fb_size()
 
     glfw.set_window_size_callback(window, window_resize_callback)
+
+    def scroll_callback(win, xoffset, yoffset):
+        scene.zoom(yoffset)
+        scene.draw_scene(fast=True)
+
+    glfw.set_scroll_callback(window, scroll_callback)
 
     while not glfw.window_should_close(window):
         if dragging:
@@ -158,19 +203,13 @@ def main():
             last_pos_y = y
 
             if dx != 0 or dy != 0:
-                world_side = np.linalg.cross(cam_pose, world_up)
-                world_side_rot = rotation_matrix_about_vector(dy / 300., world_side)
-                cam_pose = rotation_matrix_about_vector(-dx / 300., world_up) @ cam_pose
-                cam_pose = world_side_rot @ cam_pose
-                world_up = world_side_rot @ world_up
-
-                draw_scene(texture_id, fast=True)
-                glfw.swap_buffers(window)
+                scene.rotate_2d(dx, dy)
+                scene.draw_scene(fast=True)
             
         glfw.wait_events()
 
     glfw.terminate()
-    gl.glDeleteTextures(1, [texture_id])
+    del scene
 
 if __name__ == "__main__":
     main()
--- cax/_utils.py
+++ cax/_utils.py
@@ -35,40 +35,34 @@ def rotation_matrix_about_vector(angle, axis_vec):
         [z*x*C - y*s,   z*y*C + x*s, c + z*z*C]
     ])
 
-def raycast(sdf, p0, step_n, dir):
+def _dummy_sdf():
+    def _df(p):
+        return jnp.inf
+    return _df
+
+def raycast_ortho(sdf, dir, step_n, p0):
     def f(_, p):
         return p+sdf(p)*dir
     return jax.lax.fori_loop(0, step_n, f, p0)
 
-def camera_rays_perspective(forward, world_up, looking_at, view_size, fx=0.6):
-    right = jnp.cross(forward, world_up)
-    down = jnp.cross(right, forward)
-    R = normalize(jnp.vstack([right, down, forward]))
-    w, h = view_size
+def camera_rays_ortho(w, h, camera_position, camera_up, looking_at, fx):
+    cam_norm = looking_at - camera_position
+    right = jnp.cross(cam_norm, camera_up)
+    down = jnp.cross(right, cam_norm)
+    R = normalize(jnp.vstack([right, down]))
     fy = fx/w*h
     y, x = jnp.mgrid[-1.:1.:h*1j, -1.:1.:w*1j].reshape(2, -1)
     y *= fy
     x *= fx
-    return normalize(jnp.c_[x, y, jnp.ones_like(x)]) @ R
+    return jnp.c_[x, y] @ R + camera_position.reshape(1, 3)
 
-def camera_rays_ortho(forward, world_up, looking_at, view_size, fx=0.6):
-    right = jnp.cross(forward, world_up)
-    down = jnp.cross(right, forward)
-    R = normalize(jnp.vstack([right, down, forward]))
-    w, h = view_size
-    fy = fx/w*h
-    y, x = jnp.mgrid[-1.:1.:h*1j, -1.:1.:w*1j].reshape(2, -1)
-    y *= fy
-    x *= fx
-    return normalize(jnp.c_[x, y, jnp.ones_like(x)]) @ R
-
-@partial(jax.jit, static_argnums=(0, 1, 2, 3))
-def redraw(sdf, width, height, step_n, pos0, world_up, looking_at, fx, color):
-    ray_dir = camera_rays_perspective(-pos0, world_up, looking_at, view_size=(width, height), fx=fx)
-    hit_pos = jax.vmap(partial(raycast, sdf, pos0, step_n))(ray_dir)
+@partial(jax.jit, static_argnames=("sdf", "w", "h", "step_n"))
+def redraw_ortho(sdf, w, h, step_n, camera_position, camera_up, looking_at, fx):
+    ray_pos = camera_rays_ortho(w, h, camera_position, camera_up, looking_at, fx)
+    hit_pos = jax.vmap(partial(raycast_ortho, sdf, normalize(looking_at - camera_position), step_n))(ray_pos)
     mask = (jax.vmap(sdf)(hit_pos) < 1.0).astype(jnp.float32)
-    return jnp.concatenate((hit_pos.reshape(height, width, 3) % 1.0, mask.reshape(height, width, 1)), axis=-1)
-    # vis_data = jax.vmap(jax.grad(sdf))(hit_pos)
-    # # vis_data = norm(jax.vmap(jax.grad(sdf))(hit_pos))[:,jnp.newaxis] * jnp.array([[1., 1., 1.]])
-    # return vis_data.reshape(height, width, 3)
-
+    # mask = jnp.ones(hit_pos.shape[0])
+    # shading = jax.vmap(jax.grad(sdf))(hit_pos)
+    # shading = jnp.abs(shading)
+    return jnp.concatenate((hit_pos.reshape(h, w, 3) % 1.0, mask.reshape(h, w, 1)), axis=-1)
+    # return jnp.concatenate((shading, mask[:, jnp.newaxis]), axis=-1).reshape(h, w, 4)
